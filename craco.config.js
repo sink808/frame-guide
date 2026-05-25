f45bbc24da2b10b3fd7a9186bf6eb95ad3d664b5
HeimerAkousist <heimer_chung@akousist.com>
2026-05-25 13:43:13 +0800

    chore: init setting

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const CracoCSSModules = require('craco-css-modules');
+
 module.exports = {
-  style: {
-    modules: {
-      localIdentName: "[name]__[local]__[hash:base64:5]",
+  webpack: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@redux': path.resolve(__dirname, 'src/redux'),
     },
   },
+  style: {
+    sass: {
+      loaderOptions: {
+        sassOptions: {
+          includePaths: [path.resolve(__dirname, 'src')],
+          silenceDeprecations: ['legacy-js-api'],
+        },
+      },
+    },
+  },
+  plugins: [{ plugin: CracoCSSModules }],
 };
